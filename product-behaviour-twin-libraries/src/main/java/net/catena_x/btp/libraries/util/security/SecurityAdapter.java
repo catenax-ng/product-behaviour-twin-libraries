3c3e5df29e8a2bef0b361be50d1b965d796b4680
afederl <anton.federl@pfeiffercontrol.de>
2022-11-24 11:24:43 +0100

    Extended SecurityAdapter.

--- product-behaviour-twin-libraries/src/main/java/net/catena_x/btp/libraries/util/security/SecurityAdapter.java
+++ product-behaviour-twin-libraries/src/main/java/net/catena_x/btp/libraries/util/security/SecurityAdapter.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,9 +19,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityAdapter
         extends WebSecurityConfigurerAdapter //FA: For Test only!!!
 {
+    private static final String ENDPOINT_HIDATARECEIVER_NOTIFYRESULT = "/hidatareceiver/notifyresult";
+
     @Value("${security.api.username}") private String username;
     @Value("${security.api.password}") private String password;
     @Value("${security.api.role}") private String userrole;
+
+    @Value("${security.api.endpoints.hidatareceiver.notifyresult.noauth:false}")
+    private boolean hiDataReceiverNotifyresultNoAuth;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,7 +39,21 @@ public class SecurityAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+
         http.csrf().disable();
+        /*
+        .authorizeRequests()
+                .antMatchers(ENDPOINT_HIDATARECEIVER_NOTIFYRESULT)
+                .permitAll().anyRequest()
+                .authenticated();
+         */
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        if(hiDataReceiverNotifyresultNoAuth) {
+            web.ignoring().antMatchers(HttpMethod.POST, ENDPOINT_HIDATARECEIVER_NOTIFYRESULT);
+        }
     }
 
     @Bean
