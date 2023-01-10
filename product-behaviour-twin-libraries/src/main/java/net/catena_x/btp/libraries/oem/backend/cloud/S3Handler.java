64f8530cbcb55d357ac55caa36fb8fcf9d6bff0a
Benedikt Franke <benedikt.franke@dlr.de>
2023-01-10 14:55:41 +0100

    Small changes to S3Handler

--- product-behaviour-twin-libraries/src/main/java/net/catena_x/btp/libraries/oem/backend/cloud/S3Handler.java
+++ product-behaviour-twin-libraries/src/main/java/net/catena_x/btp/libraries/oem/backend/cloud/S3Handler.java
@@ -1,18 +1,28 @@
 package net.catena_x.btp.libraries.oem.backend.cloud;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.minio.*;
+import io.minio.errors.MinioException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 @Component
 @Scope("singleton")
 public class S3Handler {
-    /*
-    @Autowired @Qualifier("defaultMinio") private MinioClient client;
+    @Autowired
+    @Qualifier("defaultMinio") private MinioClient client;
     @Value("cloud.bucket") private String bucket;
 
     @Bean("defaultMinio")
@@ -29,7 +39,6 @@ public class S3Handler {
                 .region(region)
                 .build();
      }
-*/
 
     /**
      * this should be used inside of a try...with-block
@@ -39,17 +48,14 @@ public class S3Handler {
      * @throws NoSuchAlgorithmException
      * @throws InvalidKeyException
      */
-    public InputStream streamFileFromCloud(String key) throws IOException, //MinioException,
+    public InputStream streamFileFromCloud(String key) throws IOException, MinioException,
             NoSuchAlgorithmException, InvalidKeyException {
-        /*
         return client.getObject(
                 GetObjectArgs.builder()
                         .bucket(bucket)
                         .object(key)
                         .build()
         );
-        */
-        return null;
     }
 
     /**
@@ -62,18 +68,18 @@ public class S3Handler {
      * @return
      * @throws IOException
      */
-    public <T> T parseJsonFromS3(String bucketName, String key, Class<T> resultType) throws IOException, // MinioException,
+    public <T> T parseJsonFromS3(String bucketName, String key, Class<T> resultType) throws IOException, MinioException,
             NoSuchAlgorithmException, InvalidKeyException {
-        /*
         ObjectMapper om = new ObjectMapper();
         return om.readValue(streamFileFromCloud(key), resultType);
-        */
-        return null;
     }
 
-    public void uploadFileToS3(String resultJson, String key) throws IOException, // MinioException,
+    public void uploadFileToS3(String resultJson, String key) throws IOException, MinioException,
             NoSuchAlgorithmException, InvalidKeyException {
-/*        // client.putObject(bucketName, key, resultJson);
+
+        // if(!client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build()))
+        //    throw new IOException("Bucket does not exist!");
+
         var stream = new ByteArrayInputStream(resultJson.getBytes(StandardCharsets.UTF_8));
         client.putObject(PutObjectArgs.builder()
                         .bucket(bucket)
@@ -81,6 +87,22 @@ public class S3Handler {
                         .stream(stream, stream.available(), -1)
                         .contentType("application/json")
                         .build());
-*/
+    }
+
+    public void uploadAndCreate(String resultJson, String key) throws IOException, MinioException,
+            NoSuchAlgorithmException, InvalidKeyException {
+        createBucket();
+        uploadFileToS3(resultJson, key);
+    }
+
+    public void createBucket() throws IOException, MinioException,
+            NoSuchAlgorithmException, InvalidKeyException {
+        boolean found = client.bucketExists(BucketExistsArgs.builder().bucket("asiatrip").build());
+        if (!found) {
+            // Make a new bucket called 'asiatrip'.
+            client.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+        } else {
+            System.out.println("Bucket " + bucket + " already exists.");
+        }
     }
 }
