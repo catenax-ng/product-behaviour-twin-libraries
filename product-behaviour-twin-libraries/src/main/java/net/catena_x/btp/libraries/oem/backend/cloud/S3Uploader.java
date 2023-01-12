be62778e2af561326072b81866e25e2a0b964bbf
Benedikt Franke <benedikt.franke@dlr.de>
2023-01-12 11:08:34 +0100

    Implemented generation of access urls

--- product-behaviour-twin-libraries/src/main/java/net/catena_x/btp/libraries/oem/backend/cloud/S3Uploader.java
+++ product-behaviour-twin-libraries/src/main/java/net/catena_x/btp/libraries/oem/backend/cloud/S3Uploader.java
@@ -3,7 +3,10 @@ package net.catena_x.btp.libraries.oem.backend.cloud;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.minio.*;
 import io.minio.errors.*;
+import io.minio.http.Method;
 import io.minio.messages.DeleteObject;
+import net.catena_x.btp.libraries.util.exceptions.BtpException;
+import okhttp3.HttpUrl;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -14,6 +17,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /*
 Plan for Upload Module:
@@ -27,7 +31,8 @@ The main S3Handler is a simple Java Class, Spring Interactions are managed separ
 [x] deleteBucket - Deletes the bucket
 [ ] deleteKey - deletes a key (~file) inside the bucket
 [ ] Spring Wrapper - See S3SpringWrapper.java
-[ ] Generate access key for specific file
+[x] Generate url to download a specific file
+[x] Generate URL to upload a file to a specific bucket
  */
 
 public class S3Uploader {
@@ -50,18 +55,19 @@ public class S3Uploader {
      * this should be used inside of a try...with-block
      * @param key
      * @return
-     * @throws IOException
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeyException
+     * @throws BtpException
      */
-    public InputStream streamFileFromS3(String key) throws IOException, MinioException,
-            NoSuchAlgorithmException, InvalidKeyException {
-        return client.getObject(
-                GetObjectArgs.builder()
-                        .bucket(bucket)
-                        .object(key)
-                        .build()
-        );
+    public InputStream streamFileFromS3(String key) throws BtpException {
+        try {
+            return client.getObject(
+                    GetObjectArgs.builder()
+                            .bucket(bucket)
+                            .object(key)
+                            .build()
+            );
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
+        }
     }
 
     /**
@@ -74,40 +80,50 @@ public class S3Uploader {
      * @return
      * @throws IOException
      */
-    public <T> T parseJsonFromS3(String bucketName, String key, Class<T> resultType) throws IOException, MinioException,
-            NoSuchAlgorithmException, InvalidKeyException {
+    public <T> T parseJsonFromS3(String bucketName, String key, Class<T> resultType) throws BtpException {
         ObjectMapper om = new ObjectMapper();
-        return om.readValue(streamFileFromS3(key), resultType);
+        try {
+            return om.readValue(streamFileFromS3(key), resultType);
+        } catch (IOException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
+        }
+
     }
 
-    public void uploadFileToS3(String resultJson, String key) throws IOException, MinioException,
-            NoSuchAlgorithmException, InvalidKeyException {
+    public void uploadFileToS3(String resultJson, String key) throws BtpException {
 
-        // if(!client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build()))
-        //    throw new IOException("Bucket does not exist!");
+        // Maybe this method should optionally also set retention - i.e. not be deletable for a set interval of time
 
         var stream = new ByteArrayInputStream(resultJson.getBytes(StandardCharsets.UTF_8));
-        client.putObject(PutObjectArgs.builder()
-                        .bucket(bucket)
-                        .object(key)
-                        .stream(stream, stream.available(), -1)
-                        .contentType("application/json")
-                        .build());
+        try {
+            client.putObject(PutObjectArgs.builder()
+                    .bucket(bucket)
+                    .object(key)
+                    .stream(stream, stream.available(), -1)
+                    .contentType("application/json")
+                    .build());
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
+        }
+
     }
 
-    public void uploadAndCreate(String resultJson, String key) throws IOException, MinioException,
-            NoSuchAlgorithmException, InvalidKeyException {
+    public void uploadAndCreate(String resultJson, String key) throws BtpException {
         createBucket();
+
         uploadFileToS3(resultJson, key);
     }
 
-    public void createBucket() throws IOException, MinioException,
-            NoSuchAlgorithmException, InvalidKeyException {
-        boolean found = checkBucketExists();
-        if (!found) {
-            client.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
-        } else {
-            System.out.println("Bucket " + bucket + " already exists.");
+    public void createBucket() throws BtpException {
+        try {
+            boolean found = checkBucketExists();
+            if (!found) {
+                client.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+            } else {
+                System.out.println("Bucket " + bucket + " already exists.");
+            }
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
         }
     }
 
@@ -116,16 +132,54 @@ public class S3Uploader {
         return client.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
     }
 
-    public void deleteBucket(boolean allowNonEmpty) throws IOException, MinioException,
-            NoSuchAlgorithmException, InvalidKeyException {
-        if(allowNonEmpty) {
-            List<DeleteObject> keys = new ArrayList<>();
-            for(var e: client.listObjects(ListObjectsArgs.builder().bucket(bucket).build())) {
-                keys.add(new DeleteObject(e.get().objectName()));
+    public void deleteBucket(boolean allowNonEmpty) throws BtpException {
+        try {
+            if(allowNonEmpty) {
+                List<DeleteObject> keys = new ArrayList<>();
+                for(var e: client.listObjects(ListObjectsArgs.builder().bucket(bucket).build())) {
+                    keys.add(new DeleteObject(e.get().objectName()));
+                }
+                // the deletion is lazily evaluated, so as long as the result is not iterated, nothing is deleted...
+                for(var e: client.removeObjects(RemoveObjectsArgs.builder().bucket(bucket).objects(keys).build())) {}
             }
-            // the deletion is lazily evaluated, so as long as the result is not iterated, nothing is deleted...
-            for(var e: client.removeObjects(RemoveObjectsArgs.builder().bucket(bucket).objects(keys).build())) {}
+            client.removeBucket(RemoveBucketArgs.builder().bucket(bucket).build());
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
         }
-        client.removeBucket(RemoveBucketArgs.builder().bucket(bucket).build());
+    }
+
+    // Whats the best way to forward the expiry time?
+    public HttpUrl getDownloadURL(String fileName, int expiry, TimeUnit expiryTimeUnit) throws BtpException {
+        try {
+            return HttpUrl.parse(
+                    client.getPresignedObjectUrl(
+                            GetPresignedObjectUrlArgs.builder()
+                                    .method(Method.GET)
+                                    .bucket(bucket)
+                                    .object(fileName)
+                                    .expiry(expiry, expiryTimeUnit)
+                                    .build()
+                    )
+            );
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
+        }
+    }
+
+    public HttpUrl getUploadURL(String fileName, int expiry, TimeUnit expiryTimeUnit) throws BtpException {
+        try {
+            return HttpUrl.parse(
+                    client.getPresignedObjectUrl(
+                            GetPresignedObjectUrlArgs.builder()
+                                    .method(Method.PUT)
+                                    .bucket(bucket)
+                                    .object(fileName)
+                                    .expiry(expiry, expiryTimeUnit)
+                                    .build()
+                    )
+            );
+        } catch (IOException | MinioException | NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new BtpException(e.getMessage(), e.getCause());
+        }
     }
 }
